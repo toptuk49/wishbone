94b5295a8141adeeb4a6c54f8548600d3cbb0168
toptuk49 <chyka228tigra@yandex.ru>
2025-06-29 17:53:37 +0300

    feat(build): sass and css are now supported
    Style files are processed by the lightningcss. Also, the dev server will now be updated whenever the source code changes.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,27 @@
 import * as path from "node:path";
 
 import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import * as MiniCssExtractPlugin from "mini-css-extract-plugin";
+import * as CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import * as lightningcss from "lightningcss";
+import * as browserslist from "browserslist";
 
 export default {
   mode: "development",
   entry: "./src/main.ts",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    })
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin()
   ],
   devServer: {
-    static: "./dist"
+    static: {
+      directory: "./dist",
+      watch: true
+    },
+    watchFiles: ["src/**/*.html"],
+    hot: true,
   },
   module: {
     rules: [
@@ -19,6 +29,10 @@ export default {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
   },
@@ -27,6 +41,19 @@ export default {
   },
   output: {
     filename: "[contenthash:8].bundle.js",
-    path: path.resolve(__dirname, "./dist")
-  }
+    path: path.resolve(__dirname, "./dist"),
+    clean: true
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minify: CssMinimizerPlugin.lightningCssMinify,
+        minimizerOptions: {
+          // @ts-ignore
+          targets: lightningcss.browserslistToTargets(browserslist('>= 0.25%'))
+        },
+      }),
+    ],
+  },
 }
