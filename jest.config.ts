40588818d1e383845904cee3a1cfb7607dbab1fa
toptuk49 <chyka228tigra@yandex.ru>
2025-06-30 09:33:42 +0300

    feat(build): integrated jest library for testing and an alias for src folder
    Jest is configured to support testing typescript code with ES modules support.

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'jest'
+import { createDefaultEsmPreset } from 'ts-jest'
+
+const presetConfig = createDefaultEsmPreset({
+  //...options
+})
+
+export default {
+  ...presetConfig,
+} satisfies Config
